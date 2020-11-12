460924c563fc90c099a97bb2459ab7b3eee66efd
John Klay <johnandklay@gmail.com>
2020-11-12 14:38:34 -0500

    Initial commit

---
+++ src/main/java/HelpCommand.java
@@ -0,0 +1,25 @@
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+public class HelpCommand {
+    public static void helpCommand(GuildMessageReceivedEvent event) {
+        //Open DM
+        event.getAuthor().openPrivateChannel().queue((dmChannel) -> {
+            EmbedBuilder eb = new EmbedBuilder();
+
+            //Make embed
+            eb.setTitle("Help Menu");
+            eb.setAuthor("Parrot Bot", null, "https://cdn.discordapp.com/avatars/776521471392350288/43cc5b41517bea49d0d2551e3322cd84.png?size=256");
+            eb.addField("p![Message]",
+                    "Type `p!` followed by your message that you want the bot to parrot.", true);
+            eb.addField("p![Channel] [Message]",
+                    "Type `p!` followed by the channel where you want the bot to send the message, then your message that you want the bot to parrot.", true);
+
+            MessageEmbed finalEmbed = eb.build();
+
+            //Send embed
+            dmChannel.sendMessage(finalEmbed).queue();
+        });
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,89 @@
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class Main extends ListenerAdapter {
+    public static JDA api;
+    public static String token;
+
+    public static void main(String[] args) {
+        token = "Nzc2NTIxNDcxMzkyMzUwMjg4.X62F-Q.umBy158yYULr1ZJiCnnyLzW90yA";
+        try {
+            startBot();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public static boolean startBot() throws InterruptedException {
+
+        JDABuilder preBuild = JDABuilder.createDefault(token);
+        try {
+            api = preBuild.build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        api.addEventListener(new Main());
+        api.awaitReady();
+        return true;
+    }
+
+    @Override
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+        //Vars
+        Message message = event.getMessage();
+        String messageRaw = message.getContentRaw();
+        String messageNoPrefix = messageRaw.substring(2);
+        TextChannel channel = event.getChannel();
+
+        //Don't reply to bots
+        if (event.getAuthor().isBot())
+            return;
+
+        if (messageRaw.startsWith("p!")) {
+            /*
+            Role parrotControllerRole = event.getGuild().getRoleById("776528941098729473");
+
+            if (!event.getMember().getRoles().contains(parrotControllerRole))
+                return;
+                */
+
+            if (!event.getMember().hasPermission(Permission.MESSAGE_MANAGE))
+                return;
+
+            if (messageRaw.contains("p!help")) {
+                HelpCommand.helpCommand(event);
+                return;
+            }
+
+            //If no channel's mentioned
+            if (message.getMentionedChannels().isEmpty()) {
+                channel.sendMessage(messageNoPrefix).queue();
+                //If an image is attached
+                if (!message.getAttachments().isEmpty())
+//                    channel.sendMessage("").addFile(message.getAttachments());
+
+                message.delete().queue();
+            }
+            //If a channel is mentioned
+            else {
+                TextChannel channelToSendMessage = message.getMentionedChannels().get(0);
+
+                String newMessage = messageNoPrefix.replaceFirst("<#[0-9]+>", "");
+                channelToSendMessage.sendMessage(newMessage).queue();
+                message.delete().queue();
+            }
+        }
+    }
+}
